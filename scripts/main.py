5d626f9e0e2c9c669334b5761070cb48b496c314
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-09-02 15:27:12 -0400

    Files Import
    Imported csv file for the repo, added folders

---
+++ scripts/main.py
@@ -0,0 +1,13 @@
+# Installing packages 
+import pandas as pd
+import datetime as dt
+import uuid 
+import numpy as np
+
+# Importing data into variable
+slm = pd.read_csv('data\School_Learning_Modalities.csv')
+
+# Displays csv file information
+slm 
+
+
